478e0025eda9e3e483d534e68120a19a04a8c712
R14LP <alperen_loltroll@hotmail.com>
2026-03-03 23:16:36 +0300

    recursive ile kullanıcıdan alınan dizideki en küçük sayıyı bul

---
+++ pl2-6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int Smallest(int array[], int n)
+{
+    if(n == 1)
+        return array[0];
+
+    int min = Smallest(array, n - 1);
+
+    if(array[n - 1] < min)
+    {
+        return array[n - 1];
+    }else
+    {
+        return min;
+    }
+}
+
+int main()
+{
+    int n;
+    printf("how many entries:\n");
+    scanf("%d", &n);
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the %d. number:\n", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    printf("smallest number in the array:\n%d", Smallest(array, n));
+    return 0;
+}
