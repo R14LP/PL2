dcb2049a0305a1c867da68c802cb22616ec70970
R14LP <alperen_loltroll@hotmail.com>
2026-03-03 13:48:28 +0300

    recursive ile verilen sayıya kadar olan sayıların toplamı

---
+++ pl2-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int Sum(int);
+
+int main()
+{
+    int number;
+    printf("give a number to see the sum of the sequence:");
+    scanf("%d", &number);
+    printf("sum=%d", Sum(number));
+}
+
+int Sum(int number)
+{
+    if (number == 1)
+        return 1;
+    return number + Sum(number - 1);
+}
